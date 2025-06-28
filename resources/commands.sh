d68acbe22cdac12e542f8a02f090dc4bd9b6959b
Renato Haeberli <>
2025-06-28 13:09:24 +0200

    export lightgbm model with string values only

--- resources/commands.sh
+++ resources/commands.sh
@@ -5,5 +5,15 @@ curl -k -X POST -H 'Content-type:application/json'  --data-binary @solr-features
 curl -k -XDELETE https://localhost:8983/solr/master_powertools_Product_default/schema/feature-store/_DEFAULT_
 
 #see featurue-stroe
-http://localhost:8983/solr/techproducts/schema/feature-store/commonFeatureStore
+https://localhost:8983/solr/master_powertools_Product_default/schema/feature-store/commonFeatureStore
 
+
+#modle upload
+curl -k -XPUT 'https://localhost:8983/solr/master_powertools_Product_default/schema/model-store' --data-binary "lightgbm.json" -H 'Content-type:application/json'
+
+
+
+https://localhost:8983/solr/master_powertools_Product_default/select?defType=dismax&indent=true&q.op=OR&q=drill&qf=brand_string_mv%20name_text_en%20manufacturerName_text%20categoryName_text_en_mv%20description_text_en&rows=50&useParams=
+
+
+https://localhost:8983/solr/master_powertools_Product_default/select?defType=dismax&fl=code_string priceValue_usd_double score&indent=true&q.op=OR&q=drill&qf=brand_string_mv%20name_text_en%20manufacturerName_text%20categoryName_text_en_mv%20description_text_en&rows=200&rq={!ltr model=multipleadditivetreesmodel  efi.text=drill reRankDocs=100}
